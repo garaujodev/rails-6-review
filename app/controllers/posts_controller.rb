c0dbd08c4755ca2df34ce3e4aab5860e9d846c6f
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2019-08-20 12:07:04 -0300

    Gardening PostsController :sunflower:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,9 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+
     action_params = params.permit(:action)
+
     redirect_to action_params.merge(action: :show, id: 1)
   end
 
